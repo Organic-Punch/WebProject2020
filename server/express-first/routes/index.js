d34f81532ea6a5f360e3e5c93ed725352958303b
Organic-Punch <sinho667@naver.com>
2020-07-29 22:49:38 +0900

    회원가입구현

--- server/express-first/routes/index.js
+++ server/express-first/routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'React' });
+    res.render('index', { title: 'FaceBook' });
 });
 
-router.get('/join', function(req, res, next) {
-    res.render('join', { title: 'React' });
+router.get('/', function(req, res, next) {
+    res.render('join', { title: 'join' });
 });
 
 module.exports = router;
